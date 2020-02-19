e1a28b87442f5dc64feecce907f7c04526a8f166
Brian Qiu <brianlqiu5@gmail.com>
2020-02-19 14:46:59 -0800

    Added result and clear functionality, basic calculator functions completed

--- calculator.js
+++ calculator.js
@@ -15,23 +15,25 @@ function mult(x, y) {
 }
 
 function operate(op, x, y) {
+    let intX = parseInt(x);
+    let intY = parseInt(y);;
     if(op == '+') {
-        add(x, y);
+        return add(intX, intY);
     }
     if(op == '-') {
-        sub(x, y);
+        return sub(intX, intY);
     }
     if(op == '/') {
-        div(x, y);
+        return div(intX, intY);
     }
-    if(op == '*') {
-        mult(x, y);
+    if(op == 'x') {
+        return mult(intX, intY);
     }
 }
 
 function updateDisplay(e) {
     let display = document.getElementById(currInput);
-    if(display.textContent == "0") {
+    if(display.textContent == "") {
         display.textContent = e.target.textContent;
     } else {
         display.textContent += e.target.textContent;
@@ -44,9 +46,27 @@ function updateOperand(e) {
     display.textContent = e.target.textContent;
 }
 
+function getResult(e) {
+    let display = document.getElementById("result");
+    let operand = document.getElementById("opDisplay").textContent;
+    let x = document.getElementById("xDisplay").textContent;
+    let y = document.getElementById("yDisplay").textContent;
+    display.textContent = operate(operand, x, y);
+}
+
+function clear(e) {
+    currInput = "xDisplay";
+    let tbc = Array.from(document.querySelectorAll(".tbc"));
+    tbc.forEach(display => display.textContent = "");
+    let op = document.getElementById("opDisplay");
+    op.textContent = "+";
+}
+
 let currInput = "xDisplay"; 
 
 let numbers = Array.from(document.querySelectorAll(".num"));
 numbers.forEach(num => num.addEventListener("click", updateDisplay));
 let ops = Array.from(document.querySelectorAll(".op"));
 ops.forEach(op => op.addEventListener("click", updateOperand));
+document.getElementById("equals").addEventListener("click", getResult);
+document.getElementById("clear").addEventListener("click", clear);
