1d57b16adec7dae6c748ea354244c420a892defc
Brian Qiu <brianlqiu5@gmail.com>
2020-02-19 15:57:09 -0800

    Handled divide by zero errors and invalid equation forms

--- calculator.js
+++ calculator.js
@@ -15,23 +15,27 @@ function mult(x, y) {
 }
 
 function operate(op, x, y) {
-    if(op == '+') {
+    if (op == '+') {
         return add(x, y);
     }
-    if(op == '-') {
+    if (op == '-') {
         return sub(x, y);
     }
-    if(op == '/') {
+    if (op == '/') {
+        if (y == 0) {
+            alert("Invalid input: Cannot divide by 0");
+            divideByZero = true;
+        }
         return div(x, y);
     }
-    if(op == 'x') {
+    if (op == 'x') {
         return mult(x, y);
     }
 }
 
 function updateDisplay(e) {
     let display = document.getElementById("display");
-    if(newNum) {
+    if (newNum) {
         let content = document.createElement("p");
         content.classList.add("input");
         content.textContent = e.target.textContent;
@@ -43,13 +47,13 @@ function updateDisplay(e) {
         editInput.textContent += e.target.textContent;
 
     }
-    
+
 }
 
 function updateOperand(e) {
     let display = document.getElementById("display");
     let content = document.createElement("p");
-    content.classList.add("operand");;
+    content.classList.add("operand");
     content.textContent = e.target.textContent;
     display.appendChild(content);
     newNum = true;
@@ -57,29 +61,36 @@ function updateOperand(e) {
 
 
 function getResult(e) {
+    if (newNum) {
+        alert("Invalid input: Equation cannot end on an operand");
+        clear()
+    }
     let display = document.getElementById("display");
     let inputs = Array.from(document.querySelectorAll(".input"));
     let operands = Array.from(document.querySelectorAll(".operand"));
     let result = parseInt(inputs[0].textContent);
-    for(let i = 0; i < operands.length; i++) {
+    for (let i = 0; i < operands.length && !divideByZero; i++) {
         result = operate(operands[i].textContent, result, parseInt(inputs[i + 1].textContent));
     }
-    let resultDisplay = document.createElement("p");
-    resultDisplay.textContent = " = " + result;
-    display.appendChild(resultDisplay);
-    
-
-
+    if (!divideByZero) {
+        let resultDisplay = document.createElement("p");
+        resultDisplay.textContent = " = " + result;
+        display.appendChild(resultDisplay);
+    } else {
+        clear();
+    }
 }
 
-function clear(e) {
+function clear() {
     newNum = true;
     let display = document.getElementById("display");
     display.innerHTML = "";
 }
 
+
 let newNum = true; //let true if inputs are going to be new numbers
- 
+let decExists = false;
+let divideByZero = false;
 
 let numbers = Array.from(document.querySelectorAll(".num"));
 numbers.forEach(num => num.addEventListener("click", updateDisplay));
